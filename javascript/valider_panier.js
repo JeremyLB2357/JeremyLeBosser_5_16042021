18b7d581a04138bd44f80d8b5b2ce3a099f24fbb
JLBportable <jeremy.lebosser@gmail.com>
2021-05-11 06:49:30 +0200

    build: envoi formulaire fantome

--- javascript/valider_panier.js
+++ javascript/valider_panier.js
@@ -1,3 +1,5 @@
+// 1ere partie avant l'envoi de la commande à l'API
+//on récupère tous les ids présents dans le localstorage
 function recuperationPanier() {
     let arrayIdPanier = [];
     for (let i=0; i < window.localStorage.length; i++) {
@@ -6,6 +8,7 @@ function recuperationPanier() {
     return arrayIdPanier;
 }
 
+//on récupère les coordonnées notées dans le formulaire
 function recuperationCoordonnees() {
     const prenom = document.getElementById('firstname').value;
     const nom = document.getElementById('lastname').value;
@@ -22,6 +25,7 @@ function recuperationCoordonnees() {
     return personne;
 }
 
+//on met les données que l'on doit envoyer à l'API dans le bon format
 function formatageDesInfoPourAPI(personne, idsPanier) {
     let infoAEnvoyer = {
         contact: personne,
@@ -31,12 +35,8 @@ function formatageDesInfoPourAPI(personne, idsPanier) {
     return infoFormatee;
 }
 
-
-//il faut réaliser une requete post pour transmettre la réponse de L'API à la page de confirmation
-//pour cela, il faut créer un input invisible et y ajouter comme valeur la réponse de l'API
-//du coup le formulaire doit pointer vers la page de confirmation (cf: article journal du net)
-
-//on récupère l'ID dans le retour de l'API
+// 2eme partie : une fois que l'API nous a renvoyé un numéro de commande
+//on récupère l'ID dans le retour de l'API ainsi que le prix total
 function extraireIdCommande(reponseAPI) {
     const idCommande = reponseAPI.orderId;
     const produitsCommande = reponseAPI.products;
@@ -48,24 +48,9 @@ function extraireIdCommande(reponseAPI) {
     return array;
 }
 
-/*function ajoutInputFormulaire(name, value) {
-    const form = document.getElementById('formulaire-test');
-    const newInput = document.createElement('input');
-    form.appendChild(newInput);
-    newInput.setAttribute('type', 'hidden');
-    newInput.setAttribute('name', name);
-    newInput.setAttribute('value', value);
-}*/
-
-function setInputFormulaire(value) {
-    const input = document.getElementById('idcommande');
-    input.setAttribute('value', value)
-}
 
 function remplirInputFormulaire(reponseAPI) {
     const infoAEnvoyer = extraireIdCommande(reponseAPI);
-    //ajoutInputFormulaire('idcommande', infoAEnvoyer[0]);
-    //ajoutInputFormulaire('total', infoAEnvoyer[1]);
     const input1 = document.getElementById('idcommande');
     input1.setAttribute('value', infoAEnvoyer[0]);
     const input2 = document.getElementById('total');
@@ -73,16 +58,29 @@ function remplirInputFormulaire(reponseAPI) {
     console.log('formulaire rempli');
 }
 
-async function validerFormulaire() {
-    await envoyerCommande()
-    .then(document.getElementById('formulaire-test').submit())
+function remplirFormulaireFantome(reponseAPI) {
+    const infoAEnvoyer = extraireIdCommande(reponseAPI);
+    const Input = document.getElementById('idcommande');
+    const Input2 = document.getElementById('total');
+    Input.setAttribute('value', infoAEnvoyer[0]);
+    Input2.setAttribute('value', infoAEnvoyer[1]);
+    console.log('le formulaire est rempli');
+    return true;
 }
-//on envoie le tout dans l'URL de la page de confirmation.
 
+function validerFormulaireFantome(booleen) {
+    if (booleen) {
+        const form = document.getElementById('form-fantome');
+        console.log(form);
+        form.submit();
+    } else {
+        console.log('le formulaire est vide')
+    }
+}
 
 //on envoie le tout à l'API
 
-async function requetePost(info) {
+async function requeteNouvelleCommandeAPI(info) {
     await fetch('http://localhost:3000/api/teddies/order', {  
         method: 'POST',
         headers: {
@@ -90,21 +88,32 @@ async function requetePost(info) {
             },
         body: info
     })
-    .then(response => response.json())
-    .then((newResponse) => remplirInputFormulaire(newResponse))
+    .then(async response => response.json())
+    .then(ApiResponse => remplirFormulaireFantome(ApiResponse))
+    .then(newResponse => validerFormulaireFantome(newResponse))
 }
 
-async function envoyerCommande() {
+function envoyerCommande() {
     const panier = recuperationPanier();
     const contact = recuperationCoordonnees();
     const info = formatageDesInfoPourAPI(contact, panier);
-    requetePost(info);
+    requeteNouvelleCommandeAPI(info);
+    console.log('requete POST effectuée');
+    debugger;
 }
+
+/*lors du click sur le bouton valider la commande on lance le tout :
+*   on récupère les infos à envoyer
+*   on formate les info à envoyer
+*   on envoye les info à l'API
+*   une fois que l'on a une réponse, on extrait l'ID de commande de la réponse
+*   on rempli le formulaire fantome avec l'ID obtenu et le prix total
+*   une fois le formulaire rempli, on l'envoi en GET à la page de confirmation
+*/
 
 const buttonValider = document.getElementById('btn-valide-commande');
 buttonValider.addEventListener('click', function(event) {
     event.preventDefault();
-    validerFormulaire();
-    alert('la commande a été envoyée!')
+    envoyerCommande()
     }
 );
